8c9836d47552bab58448f8ff9f5fe0763f51dfe2
WJDDesigns <wayne@wjddesigns.com>
2025-07-11 19:19:38 -0400

    1.0.0-alpha2

--- dist/debug-info.js
+++ dist/debug-info.js
@@ -1,4 +1,4 @@
 // Ultra Card Debug Info
 // Version: 1.0.5
-// Build Date: 2025-07-11T20:50:57.707Z
+// Build Date: 2025-07-11T23:19:01.974Z
 // Build Mode: development
--- dist/version.js
+++ dist/version.js
@@ -1,6 +1,6 @@
 /**
  * Ultra Card Version
- * v1.0.0-alpha1
+ * v1.0.0-alpha2
  * 
  * This file is auto-generated from src/version.ts
  * DO NOT MODIFY DIRECTLY
@@ -13,6 +13,6 @@ function setVersion(value) {
 }
 
 // Set default version (will be overridden by card)
-setVersion('1.0.0-alpha1');
+setVersion('1.0.0-alpha2');
 
 export { version, setVersion };
