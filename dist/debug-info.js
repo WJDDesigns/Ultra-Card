80888526b43188b7f90e092b4a7dcd64bdfa6c93
WJDDesigns <wayne@wjddesigns.com>
2025-08-25 14:30:53 -0400

    V1.0.0-beta3

--- dist/debug-info.js
+++ dist/debug-info.js
@@ -1,4 +1,4 @@
 // Ultra Card Debug Info
-// Version: 1.0.0-beta2
-// Build Date: 2025-08-24T21:33:54.177Z
+// Version: 1.0.0-beta3
+// Build Date: 2025-08-25T18:29:35.622Z
 // Build Mode: production
