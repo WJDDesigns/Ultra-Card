2ed3dbcc00c31197d1e7e6de2cbc46ed76f7c1d9
WJDDesigns <wayne@wjddesigns.com>
2026-01-06 12:59:30 -0500

    v2.3.0-beta10: Custom Variables & Module ID improvements

--- dist/debug-info.js
+++ dist/debug-info.js
@@ -1,4 +1,4 @@
 // Ultra Card Debug Info
-// Version: 2.3.0-beta11
-// Build Date: 2026-01-06T17:56:38.984Z
+// Version: 2.3.0-beta10
+// Build Date: 2026-01-06T17:59:22.421Z
 // Build Mode: production
